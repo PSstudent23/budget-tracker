4d28eafbfcd1a606cbdc26f03b90bf3e379f14ef
Patrik <89241250@student.upr.si>
2026-06-30 21:42:17 +0200

    website fix

--- workspace/my-node-backend/src/index.ts
+++ workspace/my-node-backend/src/index.ts
@@ -2,6 +2,7 @@ import "dotenv/config";
 import express, { Request, Response } from "express";
 import mysql from "mysql2/promise";
 import cors from "cors";
+import path from "path"
 import session from "express-session";
 import dotenv from "dotenv";
 import usersRouter from "./routes/users.routes.js";
@@ -9,19 +10,20 @@ import transactionsRouter from "./routes/transactions.routes.js";
 import budgetsRouter from "./routes/budgets.routes.js";
 import goalsRouter from "./routes/goals.routes.js";
 import notificationsRouter from "./routes/notifications.routes.js";
+import { fileURLToPath } from "url";
 import { getCategories } from "./db/database.js";
-
 
 
 dotenv.config();
 
 const app = express();
-const port = Number(process.env.PORT) || 3000;
+const port = Number(process.env.PORT) || 30040;
 
 app.use(cors({
   origin: "http://localhost:30041",
   credentials: true,
 }));
+
 
 app.use(
   session({
@@ -45,7 +47,7 @@ app.get("/api", async (_req: Request, res: Response) => {
   let conn;
 
   try {
-    conn = await mysql.createConnection(process.env.DATABASE_URL!);
+    conn = await mysql.createConnection("mysql://studenti:S039C8R7@localhost:3306/SISIII2026_89241250");
 
     const [db] = await conn.query<any[]>("SELECT DATABASE() AS db");
     const [tables] = await conn.query("SHOW TABLES");
@@ -69,6 +71,8 @@ app.use("/api/budgets", budgetsRouter)
 app.use("/api/goals", goalsRouter)
 
 app.use("/api/notifications", notificationsRouter)
+//Login and Register
+app.use("/api/users", usersRouter);
 
 app.get("/api/categories", async (req: Request, res: Response) => {
   if (!req.session.user) {
@@ -92,8 +96,6 @@ app.get("/api/categories", async (req: Request, res: Response) => {
   }
 });
 
-//Login and Register
-app.use("/api/users", usersRouter);
 
 app.get("/api/me", (req: Request, res: Response) => {
   if (!req.session.user) {
@@ -119,6 +121,21 @@ app.post("/api/logout", (req: Request, res: Response) => {
     res.json({ ok: true });
   });
 });
+
+
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const reactBuildPath = __dirname;
+
+
+app.use(express.static(reactBuildPath));
+
+app.get("/*splat", (req, res) => {
+  res.sendFile(path.join(reactBuildPath, "index.html"));
+});
+
 
 app.listen(port, "0.0.0.0", () => {
   console.log(`Server running on port ${port}`);
